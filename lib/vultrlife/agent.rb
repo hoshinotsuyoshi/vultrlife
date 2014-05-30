fe9f424143a727d15b1af36639984fc11b0ba4a6
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2014-05-30 20:57:08 +0900

    Refactor GET methods

--- lib/vultrlife/agent.rb
+++ lib/vultrlife/agent.rb
@@ -4,84 +4,59 @@ require 'net/https'
 module Vultrlife
   module Agent
     API_HOST = 'https://api.vultr.com'
-    def self.fetch_all_plans
-      #/v1/plans/list
-      #GET - public
 
+    def self.query(option)
+      return '' if option.empty?
+      '?' + option.map{|e| e.join '='}.join('&')
+    end
+
+    def self.http_get(endpoint, params={})
       response = ''
-      endpoint = '/v1/plans/list'
+
       uri = URI.parse("#{API_HOST}#{endpoint}")
       https = Net::HTTP.new(uri.host, 443)
       https.use_ssl = true
       https.start{|https|
-        response = https.get(uri.path)
+        response = https.get(uri.path + query(params))
       }
 
       JSON.parse(response.body)
+    end
+
+    def self.fetch_all_plans
+      #/v1/plans/list
+      #GET - public
+
+      self.http_get('/v1/plans/list')
     end
 
     def self.fetch_all_regions
       #/v1/regions/list
       #GET - public
 
-      response = ''
-      endpoint = '/v1/regions/list'
-      uri = URI.parse("#{API_HOST}#{endpoint}")
-      https = Net::HTTP.new(uri.host, 443)
-      https.use_ssl = true
-      https.start{|https|
-        response = https.get(uri.path)
-      }
-
-      JSON.parse(response.body)
+      self.http_get('/v1/regions/list')
     end
 
     def self.fetch_all_oss
       #/v1/os/list
       #GET - public
 
-      response = ''
-      endpoint = '/v1/os/list'
-      uri = URI.parse("#{API_HOST}#{endpoint}")
-      https = Net::HTTP.new(uri.host, 443)
-      https.use_ssl = true
-      https.start{|https|
-        response = https.get(uri.path)
-      }
-
-      JSON.parse(response.body)
+      self.http_get('/v1/os/list')
     end
 
     def self.fetch_availability(dcid)
       #/v1/regions/availability
       #GET - public
 
-      response = ''
-      endpoint = "/v1/regions/availability"
-      uri = URI.parse("#{API_HOST}#{endpoint}")
-      https = Net::HTTP.new(uri.host, 443)
-      https.use_ssl = true
-      https.start{|https|
-        response = https.get(uri.path + "?DCID=#{dcid}")
-      }
-
-      JSON.parse(response.body).map(&:to_s)
+      self.http_get('/v1/regions/availability', DCID: dcid)
+      .map(&:to_s)
     end
 
     def self.fetch_server_list(api_key)
       #/v1/server/list
       #GET - account
 
-      response = ''
-      endpoint = "/v1/server/list"
-      uri = URI.parse("#{API_HOST}#{endpoint}")
-      https = Net::HTTP.new(uri.host, 443)
-      https.use_ssl = true
-      https.start{|https|
-        response = https.get(uri.path + "?api_key=#{api_key}")
-      }
-
-      JSON.parse(response.body)
+      self.http_get('/v1/server/list', api_key: api_key)
     end
 
     def self.post_create(option)
